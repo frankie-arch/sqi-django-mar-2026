ce32b7307f0b570a9e4eedd8db5168574415cf2b
maxin <ezebuefrancia@gmail.com>
2026-03-14 02:05:53 +0100

    add creative gh

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "core" 
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('services', views.services, name='services'),
+    path('about', views.about, name='about'),
+]
---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
+def about(request):
+    return render(request, 'core/about.html')
